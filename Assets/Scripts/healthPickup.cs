476b77f5757aa501c969b23ef9549b0349e9c144
Ryan <ryanaustinfree@gmail.com>
2016-12-05 19:37:26 -0500

    New Features: - Item now makes a noise when you pick it up.

--- Assets/Scripts/healthPickup.cs
+++ Assets/Scripts/healthPickup.cs
@@ -6,10 +6,17 @@ public class healthPickup : MonoBehaviour {
 
     public float healthAmount;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //audio variables
+    public AudioClip playerCollect;
+
+    AudioSource playerAS;
+    SpriteRenderer renderer;
+
+    // Use this for initialization
+    void Start () {
+        playerAS = GetComponent<AudioSource>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,9 +27,13 @@ public class healthPickup : MonoBehaviour {
     {
         if(other.tag == "Player")
         {
+            renderer.enabled = false; //once collected, hide the sprite
+            
+            playerAS.PlayOneShot(playerCollect); //play sound
             playerHealth theHealth = other.gameObject.GetComponent<playerHealth>();
-            theHealth.addHealth(healthAmount);
-            Destroy(gameObject);
+
+            theHealth.addHealth(healthAmount); //increase health
+            Destroy(gameObject, playerCollect.length); //destroy after sound is done
         }
     }
 }
