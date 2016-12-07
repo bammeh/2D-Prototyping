c29e0c775d4538ce9ca7b39b4ba2301bee0bcbf2
Ryan <ryanaustinfree@gmail.com>
2016-12-06 23:16:37 -0500

    New Features: Added Joystick and Buttons for mobile.
    There will be bugs, but I'll iron those out soon.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class playerController : MonoBehaviour {
+public class playerController : MonoBehaviour
+{
 
     //movement variables
     public float maxSpeed;
@@ -18,6 +20,8 @@ public class playerController : MonoBehaviour {
     Animator myAnim;
     SpriteRenderer mySR;
     bool facingRight;
+    bool rightButtonPressed;
+    bool leftButtonPressed;
 
     //for shooting
     public Transform gunTip;
@@ -26,18 +30,61 @@ public class playerController : MonoBehaviour {
     float nextFire = 0f;
 
 
-	void Start () {
+
+    void Start()
+    {
         myRB = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
         mySR = GetComponent<SpriteRenderer>();
 
         facingRight = true;
-        
-	}
+
+    }
+
     //Once Per Frame
     void Update()
     {
-        if(grounded && Input.GetAxis("Jump") > 0)
+        /*if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnim.SetBool("isGrounded", grounded);
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
+
+        // Player shooting
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            fireRocket();
+        }*/
+    }
+
+    void FixedUpdate()
+    {
+
+        //Check if we are running either in the Unity editor or in a standalone build.
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+        //NEEDED FOR PROPER ANIMATION - check if we are grounded. If no, then we are falling.
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        myAnim.SetBool("isGrounded", grounded);
+
+        myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+
+
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            flip();
+        }
+
+                if (grounded && Input.GetAxis("Jump") > 0)
         {
             grounded = false;
             myAnim.SetBool("isGrounded", grounded);
@@ -49,32 +96,62 @@ public class playerController : MonoBehaviour {
         {
             fireRocket();
         }
-    }
-	
-	
-	void FixedUpdate () {
 
-        //check if we are grounded. If no, then we are falling.
+//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+
+        /*//JUMPING
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnim.SetBool("isGrounded", grounded);
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
+        */
+
+        //NEEDED FOR PROPER ANIMATION - check if we are grounded. If no, then we are falling.
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         myAnim.SetBool("isGrounded", grounded);
+        myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
 
-        myAnim.SetFloat("verticalSpeed",myRB.velocity.y);
+
+        //Movement Script
+        float moveX = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 moveVec = new Vector2(moveX,CrossPlatformInputManager.GetAxis("Vertical")) * maxSpeed;
+
+        myAnim.SetFloat("speed", Mathf.Abs(moveX));
+        myRB.velocity = new Vector2(moveX * maxSpeed, myRB.velocity.y);
 
 
-        float move = Input.GetAxis("Horizontal");
-        myAnim.SetFloat("speed",Mathf.Abs(move));
+        //New Jumping
+        bool isJumping = CrossPlatformInputManager.GetButton("Jump");
 
-        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+        if (grounded && isJumping)
+        {
+            grounded = false;
+            myAnim.SetBool("isGrounded", grounded);
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
 
-        if (move > 0 && !facingRight)
+
+        if (moveX > 0 && !facingRight)
         {
             flip();
-        } else if (move < 0 && facingRight)
+        }
+        else if (moveX < 0 && facingRight)
         {
             flip();
         }
 
+        //Shooting
+        bool isShooting = CrossPlatformInputManager.GetButton("Shoot");
 
+        if (isShooting)
+        {
+            fireRocket();
+        }
+        #endif //End of mobile platform dependendent compilation section started above with #elif
     }
     void flip()
     {
@@ -88,13 +165,14 @@ public class playerController : MonoBehaviour {
 
     void fireRocket()
     {
-        if(Time.time > nextFire)
+        if (Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             if (facingRight)
             {
-                Instantiate(bullet,gunTip.position,Quaternion.Euler(new Vector3(0,0,0)));
-            } else if (!facingRight)
+                Instantiate(bullet, gunTip.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+            }
+            else if (!facingRight)
             {
                 Instantiate(bullet, gunTip.position, Quaternion.Euler(new Vector3(0, 0, 180)));
             }
